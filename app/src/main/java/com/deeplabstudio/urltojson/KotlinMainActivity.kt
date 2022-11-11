3a138a48811fecf39e9cdf9184edfd41b4a44cfc
Uğur <ugur.altnsy@gmail.com>
2022-11-11 19:12:51 +0300

    Latest Version

--- app/src/main/java/com/deeplabstudio/urltojson/KotlinMainActivity.kt
+++ app/src/main/java/com/deeplabstudio/urltojson/KotlinMainActivity.kt
@@ -13,27 +13,5 @@ class KotlinMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_main)
-
-        val property = HashMap<String, String>()
-        property["X-PCK"] = "API_KEY"
-        property["User-Agent"] = "Mozilla/5.0"
-        property["Content-type"] = "application/json"
-
-        UrlJson.load("Source Url").addProperty(property).getObject(object : JsonObject {
-            @Throws(JSONException::class)
-            override fun Result(jsonObject: JSONObject) {
-            }
-
-            override fun Error(error: String) {}
-        })
-
-        UrlJson.load("Source Url").getArray(object : JsonArray {
-            @Throws(JSONException::class)
-            override fun Result(jsonArray: JSONArray) {
-            }
-
-            override fun Error(error: String) {}
-        })
-
     }
 }
